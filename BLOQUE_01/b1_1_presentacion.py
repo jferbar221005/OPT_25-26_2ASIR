484c2ce251ad2a2ddd72bed016fe587a4a585c63
pepefb2210 <pepefb2210@gmail.com>
2025-10-24 10:45:40 +0200

    commit b1

--- BLOQUE_01/b1_1_presentacion.py
+++ BLOQUE_01/b1_1_presentacion.py
@@ -1,4 +1,4 @@
 nombre = input("Introduce tu nombre ")
 edad = input("Que edad tienes ")
 
-print(f"Hola {nombre}, tienes {edad} años. Bienvenido a Python ")
+print(f"Hola {nombre}, tienes {edad} años. Te damos la bienvenida a Python ")
