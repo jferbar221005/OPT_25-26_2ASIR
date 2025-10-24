60b2fff97b2015190b36adb0677112ec94decd1f
pepefb2210 <pepefb2210@gmail.com>
2025-10-24 10:50:15 +0200

    commit b1

---
+++ BLOQUE_01/b1_2_calculadora_simple.py
@@ -0,0 +1,16 @@
+num1 = 9
+num2 = 3
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+modulo = num1 % num2
+potencia = num1 ** num2
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+print(f"Módulo: {modulo}")
+print(f"Potencia: {potencia}")
