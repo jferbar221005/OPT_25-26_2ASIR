014875e40c01ca63a70b9ca3d741afc120075d13
pepefb2210 <pepefb2210@gmail.com>
2025-10-24 11:36:56 +0200

    commit b1

---
+++ BLOQUE_01/b1_4_login.py
@@ -0,0 +1,10 @@
+usuario_correcto = "pepefb2210"
+contrasena_correcta = "2005"
+
+usuario = input("Ingrese el nombre de usuario: ")
+contrasena = input("Ingrese la contraseña: ")
+
+if usuario == usuario_correcto and contrasena == contrasena_correcta:
+    print("Acceso concedido")
+else:
+    print("Acceso denegado")
