7529f987ed2ee2be19b2f07abf864dd631e33465
pepefb2210 <pepefb2210@gmail.com>
2025-10-24 10:52:55 +0200

    commit b1

---
+++ BLOQUE_01/b1_3_conversor_datos.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+altura = float(input("¿Cuál es tu altura en metros? "))
+
+print(f"Hola, me llamo {nombre}, tengo {edad} años y mido {altura} metros.")
